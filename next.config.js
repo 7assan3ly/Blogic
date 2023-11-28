3ebf9c8ea3cd17746c0b2b455d15f39005ae410d
unknown <7assan3ly07@gmail.com>
2023-11-29 00:10:28 +0200

    second commit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
         domains: [
             "lh3.googleusercontent.com" , "firebasestorage.googleapis.com"
         ]
-    }
+    },
+    eslint: { ignoreDuringBuilds: true }
 }
 
 module.exports = nextConfig
